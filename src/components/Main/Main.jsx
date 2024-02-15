544d2721852a8a7a2960726fe6eb6e47d8ea3061
Yuvaraj643 <vyuvarajnani2001@gmail.com>
2024-02-15 22:54:17 +0530

    first commit

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
+import { Button } from "@nextui-org/react";
+import "./Main.css";
+import "animate.css";
+const Main = () => {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from API
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://yuvaraj643.github.io/Job-Slayer-API/job.json"
+        );
+        const data = await response.json();
+        setJobs(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <Card className="py-2md:w-5/6 sm:w-4/6 mx-auto">
+        <CardHeader className="pb-0 pt-2 px-4 flex-col items-center">
+          <p className="text-3xl uppercase font-bold">Job Opportunities</p>
+          <h4 className="font-bold text-large">Available Positions</h4>
+        </CardHeader>
+        <CardBody className="overflow-visible py-2 sm:w-100 md:w-5/6 mx-auto">
+          <div className="grid grid-cols-1 lg:grid-cols-1 sm:grid-cols-1 gap-5">
+            {jobs.map((job) => (
+              <div key={job.id}>
+                <div className="card animate__animated animate__fadeInDown">
+                  <img
+                    src={job.image_url}
+                    className="card-img-top"
+                    alt={job.company_name}
+                  />
+                  <div className="card-body text-lg">
+                    <span className="flex">
+                      <h2 className="font-bold text-blue-400">Company Name :</h2>
+                      <h5 className="card-title">{job.company_name}</h5>
+                    </span>
+                    <span className="flex"><h2 className="font-bold text-blue-400">Position :</h2><p className="card-text">{job.position}</p></span>
+                    <span className="flex"><h2 className="font-bold text-blue-400">Qualifications :</h2><p className="card-text">
+                      {job.qualifications} | {job.experience} | {job.location}
+                    </p></span>
+                    <div class="flex justify-center items-center p-2">
+                      <Button
+                        className="p-5 font-bold"
+                        color="primary"
+                        variant="shadow"
+                      >
+                        APPLY NOW
+                      </Button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </CardBody>
+      </Card>
+    </>
+  );
+};
+
+export default Main;
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+const {nextui} = require("@nextui-org/react");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // ...
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/react/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+};
