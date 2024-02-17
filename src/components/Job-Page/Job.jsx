9b3951e05e4e64efed9e9c217ca9285899540611
Yuvaraj643 <vyuvarajnani2001@gmail.com>
2024-02-17 20:34:36 +0530

    now

--- src/components/Job-Page/Job.jsx
+++ src/components/Job-Page/Job.jsx
@@ -190,7 +190,7 @@ const Job = () => {
                 </div>
 
                 <div class="flex justify-center items-center">
-                  <a href={job.apply_url} target="_blank">
+                  <a href={job.apply_url}>
                     <button class="flex align-center justify-center bg-gray-950 text-gray-400 border border-gray-400 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group">
                       <span class="bg-gray-400 shadow-gray-400 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
                       Apply Now
